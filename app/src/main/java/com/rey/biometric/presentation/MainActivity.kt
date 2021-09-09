62214777c3212e98bf6a97355e65bb2d7c3451c3
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-09 18:04:01 +0700

    feat: initial commit

---
+++ app/src/main/java/com/rey/biometric/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rey.biometric.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rey.biometric.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
