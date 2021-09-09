9923d02d5d6f71adfed380f5dac25b12a3426d85
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-09 19:00:21 +0700

    refactor!: change package name

--- app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
@@ -1,13 +1,13 @@
-package com.rey.biometric.presentation.fragment
+package com.rey.dumbdumb.presentation.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rey.biometric.R
+import com.rey.dumbdumb.R
 
-class AuthenticationFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
