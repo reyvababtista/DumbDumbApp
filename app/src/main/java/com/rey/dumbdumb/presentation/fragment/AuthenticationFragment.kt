6e80234de37181c3eed04e930cb9725ea5f1ec88
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-10 11:35:00 +0700

    feat: add biometric authentication

--- app/src/main/java/com/rey/dumbdumb/presentation/fragment/AuthenticationFragment.kt
+++ app/src/main/java/com/rey/dumbdumb/presentation/fragment/AuthenticationFragment.kt
@@ -1,18 +1,128 @@
 package com.rey.dumbdumb.presentation.fragment
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.*
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.rey.dumbdumb.R
+import androidx.navigation.fragment.findNavController
+import com.rey.dumbdumb.databinding.FragmentAuthenticationBinding
 
 class AuthenticationFragment : Fragment() {
 
+    private var _binding: FragmentAuthenticationBinding? = null
+    private val binding: FragmentAuthenticationBinding get() = _binding!!
+
+    private val bitwiseAuthenticationCombination =
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+        else BIOMETRIC_STRONG or BIOMETRIC_WEAK
+    private lateinit var biometricPromptInfo: BiometricPrompt.PromptInfo
+    private lateinit var biometricPrompt: BiometricPrompt
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_authentication, container, false)
+    ): View {
+        _binding = FragmentAuthenticationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBiometric()
+        initBiometricPromptInfo()
+        initBiometricPrompt()
+    }
+
+    private fun initBiometricPrompt() {
+        biometricPrompt = BiometricPrompt(
+            this,
+            ContextCompat.getMainExecutor(requireContext()),
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication error: $errString",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication succeeded!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    findNavController().navigate(AuthenticationFragmentDirections.actionAuthenticationFragmentToNavHome())
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+
+        binding.buttonLogin.setOnClickListener {
+            biometricPrompt.authenticate(biometricPromptInfo)
+        }
+    }
+
+    private fun initBiometricPromptInfo() {
+        biometricPromptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Login")
+            .setSubtitle("Subtitle")
+            .setNegativeButtonText("Noice")
+            .setAllowedAuthenticators(bitwiseAuthenticationCombination).build()
+    }
+
+    private fun initBiometric() {
+        val biometricManager = BiometricManager.from(requireContext())
+        when (biometricManager.canAuthenticate(bitwiseAuthenticationCombination)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Log.e("MY_APP_TAG", "No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                Log.e("MY_APP_TAG", "BIOMETRIC_ERROR_NONE_ENROLLED")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    // Prompts the user to create credentials that your app accepts.
+                    val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                        putExtra(
+                            Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                            BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+                        )
+                    }
+                    startActivityForResult(enrollIntent, 1)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rey.dumbdumb.R
+import com.rey.dumbdumb.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_authentication, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
