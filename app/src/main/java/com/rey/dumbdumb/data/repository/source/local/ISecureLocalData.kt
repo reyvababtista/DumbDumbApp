29163502ce8c08d3ca9d73648e106f7766197db1
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-14 15:01:09 +0700

    feat: add secure local data

--- app/src/main/java/com/rey/dumbdumb/data/repository/source/local/ISecureLocalData.kt
+++ app/src/main/java/com/rey/dumbdumb/data/repository/source/local/ISecureLocalData.kt
@@ -1,8 +1,10 @@
 package com.rey.dumbdumb.data.repository.source.local
 
 import com.rey.dumbdumb.domain.dto.EncryptReq
+import com.rey.dumbdumb.domain.dto.EncryptRes
 import com.rey.lib.cleanarch.domain.dto.Result
 
 internal interface ISecureLocalData {
     suspend fun storeCredential(data: EncryptReq): Result<Unit>
+    suspend fun getCredential(): Result<EncryptRes>
 }
