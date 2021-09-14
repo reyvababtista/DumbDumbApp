793cc1d1c283e6770016ac35698ebbe4f0cf99ae
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-14 18:00:36 +0700

    feat: implement UI changes

--- app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/rey/dumbdumb/presentation/fragment/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.rey.dumbdumb.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding get() = _binding!!
+    private val args: HomeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +20,12 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textHome.text = "Logged in, ${args.name}"
     }
 
     override fun onDestroyView() {
