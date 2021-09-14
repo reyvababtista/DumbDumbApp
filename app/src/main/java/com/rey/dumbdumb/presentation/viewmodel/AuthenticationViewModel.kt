0bec5786b8cfe97a853961cc79c2bf7f4a3ecc8c
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-14 18:00:05 +0700

    feat: implement auth view model

--- app/src/main/java/com/rey/dumbdumb/presentation/viewmodel/AuthenticationViewModel.kt
+++ app/src/main/java/com/rey/dumbdumb/presentation/viewmodel/AuthenticationViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.rey.dumbdumb.domain.entity.usecase.ISecureUseCase
+import com.rey.dumbdumb.DumbDumbApp.Companion.TAG
+import com.rey.dumbdumb.domain.dto.EncryptReq
+import com.rey.dumbdumb.domain.dto.EncryptRes
+import com.rey.dumbdumb.domain.entity.usecase.IAuthUseCase
 import com.rey.lib.cleanarch.domain.dto.onError
 import com.rey.lib.cleanarch.domain.dto.onSuccess
 import com.rey.lib.cleanarch.domain.usecase.external.ICoroutineProvider
@@ -15,18 +18,65 @@ import javax.inject.Inject
 
 internal class AuthenticationViewModel @Inject constructor(
     private val coroutine: ICoroutineProvider,
-    private val useCase: ISecureUseCase
+    private val useCase: IAuthUseCase
 ) : ViewModel() {
 
-    private val _cipher = MutableLiveData<Cipher>()
-    val cipher: LiveData<Cipher> = _cipher
+    private val _token = MutableLiveData<String?>()
+    val token: LiveData<String?> = _token
 
-    fun getChiper() = viewModelScope.launch(coroutine.default()) {
-        useCase.generateCipher()
-            .onSuccess {
-                _cipher.postValue(it)
-            }.onError {
-                Log.e("MY_APP_TAG", "getChiper: ${it}")
+    fun authenticate(username: String, password: String) = viewModelScope.launch(coroutine.io()) {
+        useCase.authenticate(username, password)
+            .onSuccess { _token.postValue(it) }
+            .onError {
+                _token.postValue(null)
+                Log.e(TAG, "authenticate: $it")
             }
+    }
+
+    private val _encryptionCipher = MutableLiveData<Cipher>()
+    val encryptionCipher: LiveData<Cipher> = _encryptionCipher
+
+    fun getEncryptionCipher() = viewModelScope.launch(coroutine.default()) {
+        useCase.getEncryptionCipher()
+            .onSuccess { _encryptionCipher.postValue(it) }
+            .onError { Log.e(TAG, "getChiper: $it") }
+    }
+
+    private val _encryptedCredential = MutableLiveData<EncryptRes?>()
+    val encryptedCredential: LiveData<EncryptRes?> = _encryptedCredential
+
+    fun getEncryptedCredential() = viewModelScope.launch(coroutine.io()) {
+        useCase.getEncryptedCredential()
+            .onSuccess { _encryptedCredential.postValue(it) }
+            .onError {
+                _encryptedCredential.postValue(null)
+                Log.e(TAG, "getEncryptedCredential: $it")
+            }
+    }
+
+    private val _decryptionCipher = MutableLiveData<Cipher>()
+    val decryptionCipher: LiveData<Cipher> = _decryptionCipher
+
+    fun getDecryptionCipher(initializationVector: ByteArray) =
+        viewModelScope.launch(coroutine.default()) {
+            useCase.getDecryptionCipher(initializationVector)
+                .onSuccess { _decryptionCipher.postValue(it) }
+                .onError { Log.e(TAG, "getChiper: $it") }
+        }
+
+    private val _name = MutableLiveData<String>()
+    val name: LiveData<String> = _name
+
+    fun authenticate() = viewModelScope.launch(coroutine.io()) {
+        useCase.authenticate(
+            encryptionCipher = encryptionCipher.value,
+            decryptionCipher = decryptionCipher.value,
+            token = token.value,
+            encryptedCredential = encryptedCredential.value?.let {
+                EncryptReq(it.ciphertext, it.initializationVector)
+            }
+        )
+            .onSuccess { _name.postValue(it) }
+            .onError { Log.e(TAG, "authenticate: $it") }
     }
 }
